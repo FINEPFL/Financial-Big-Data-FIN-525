94f98d3a8c1a9a582ec95011a8624a1a542c53d6
System Administrator <mengjie.zhao@outlook.com>
2016-12-01 22:36:36 +0100

    add property verification and statis

---
+++ src/property_verity.r
@@ -0,0 +1,56 @@
+rm(list=ls())
+
+source("init.r")
+pwd = init()
+
+us_stock_data = "/Users/mzhao/Desktop/FBD/Financial-Big-Data-FIN-525/datasets/mid-results/us_stocks/us_stocks_filtered.rds"
+mtgov_path = "/Users/mzhao/Desktop/FBD/Financial-Big-Data-FIN-525/datasets/mtgoxUSD.csv"
+
+test = 0
+
+if (test) {
+
+    mtgov = fread(mtgov_path)
+    setnames(mtgov, c("timestamp","price","volume"))
+    mtgov$timestamp = as.POSIXct(mtgov$timestamp,origin = "1970-01-01")
+    logreturn = diff(log(mtgov$price))
+    r = diff(mtgov$price)
+    logreturn
+    plot(density(logreturn), log = "y")
+
+  } else {
+
+    dataset = as.xts(readRDS(us_stock_data))
+    stock_names = names(dataset)
+    time_steps = index(dataset)
+
+    logreturn = diff(log(dataset$GE))
+    logreturn = logreturn[2:length(logreturn)]
+    r = diff(dataset$GE)
+    r = r[2:length(r)]
+    plot(density(logreturn), log = "y")
+
+}
+acf(r)
+acf(logreturn)
+
+# check cdf of logreturn
+# cdfval = ecdf(logreturn)
+# cdf_cons1 = sort(unique(logreturn))
+# plot(abs(cdf_cons1), 1-cdfval(abs(cdf_cons1)), log="xy", t="l")
+
+# sig = abs(logreturn)
+# acfval = acf(sig, plot = FALSE, log.max = 100)
+# plot(0:(length(acfval$acf)-1),acfval$acf,log='xy',t='l')
+# y1=tail(log(acfval$acf),-1)
+# lm(y1~cdf_cons1)
+
+# check market performance
+sp500 = fread("/Users/mzhao/Desktop/FBD/Financial-Big-Data-FIN-525/datasets/sp500.csv")
+sp500 = diff(sp500$Close)
+plot(density(sp500))
+theory = rnorm(10*length(sp500), mean = 0, sd=1)
+plot(density(theory))
+
+autostd_sp500 = acf(rollapplyr(sp500, 2, sd, fill = 0))
+autostd_stock = acf(rollapplyr(logreturn, 5, sd, fill = 0))
---
+++ src/stats_of_us_stocks.r
@@ -0,0 +1,39 @@
+pwd = init()
+raw_data = as.xts(get(load("../datasets/us_stocks.rda")))
+
+num_not_na = apply(raw_data, 1, function(line) {sum(!is.na(line))})
+total_nas = apply(raw_data, 1, function(line) {sum(is.na(line))})
+
+num_not_na = xts(num_not_na,index(raw_data))
+total_nas = xts(total_nas, index(raw_data))
+
+# only consider a small set of full the full us dataset
+# as the starting year, ie, 1950 is roughly full of NAs
+raw_data = raw_data['1985::']
+
+# filter require parts
+num_not_na = num_not_na[index(raw_data)]
+
+weekends_selector = num_not_na < 100
+weekends = index(raw_data)[weekends_selector]
+weekdays = index(raw_data)[!weekends_selector]
+
+wendsTick = lapply(weekends, function(tick)             {colnames(raw_data)[!is.na(raw_data[tick])]})
+wendsTick = unique(unlist(wendsTick))
+
+raw_data = raw_data[ ,!(colnames(raw_data) %in% wendsTick)]
+raw_data = raw_data[index(weekdays)]
+
+allNAdays = apply(raw_data, 1, function(row) all(is.na(row)))
+raw_data = raw_data[!allNAdays]
+
+num_stocks = length(names(raw_data))
+num_dates = length(index(raw_data))
+
+begin_date = index(raw_data)[1]
+end_date = index(raw_data)[length(index(raw_data))]
+
+saveRDS(raw_data,file='/Users/mzhao/Desktop/FBD/Financial-Big-Data-FIN-525/datasets/mid-results/us_stocks/us_stocks_filtered.rds')
+
+stats = list(num_stocks, num_dates, begin_date, end_date)
+write.table(stats, file = "/Users/mzhao/Desktop/FBD/Financial-Big-Data-FIN-525/stats/us_stock_stats", sep=",")
