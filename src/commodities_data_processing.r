c22eb5e3a12eee8e6133527f1259678b9b3d6158
System Administrator <mengjie.zhao@outlook.com>
2016-12-09 16:39:49 +0100

    remove unused files

--- src/commodities_data_processing.r
+++ src/commodities_data_processing.r
@@ -24,28 +24,28 @@ getComData = function(){
     shanghai_al = data.frame(read.csv("../datasets/commodities/shanghai_al.csv",
                                header = TRUE, sep = ",", quote = "\"",
                                dec = ".", fill = TRUE))[,c("Date","Settle")]
-    shanghai_copper = data.frame(read.csv("../datasets/commodities/shanghai_copper.csv",
-                                      header = TRUE, sep = ",", quote = "\"",
-                                      dec = ".", fill = TRUE))[,c("Date","Settle")]
+
     euronext = data.frame(read.csv("../datasets/commodities/euronext.csv",
                                       header = TRUE, sep = ",", quote = "\"",
                                       dec = ".", fill = TRUE))[,c("Date","Settle")]
- 
+
     min_len = min(length(natural_gas$Date), length(rice$Date), length(coffe$Date),
                   length(sugar$Date), length(plat$Date), length(gold$Date),
-                  length(corn$Date), length(shanghai_al$Date), length(euronext$Date),
-                  length(shanghai_copper$Date))
+                  length(corn$Date), length(shanghai_al$Date), length(euronext$Date))
 
-    retVal = data.frame(shanghai_al$Date, natural_gas$Settle[1:min_len], rice$Settle[1:min_len],
+    retVal = data.frame(natural_gas$Settle[1:min_len], rice$Settle[1:min_len],
                         coffe$Settle[1:min_len], sugar$Settle[1:min_len],
                         plat$Settle[1:min_len], gold$Settle[1:min_len],
                         corn$Settle[1:min_len], shanghai_al$Settle[1:min_len],
-                        euronext$Settle[1:min_len], shanghai_copper$Settle[1:min_len])
-  
-     setnames(retVal,c("Date", "natrual_gas","rice","coffe", "sugar", "plat", "gold", "corn",
-                      "shanghai_al", "euronext", "shanghai_copper"))
+                        euronext$Settle[1:min_len])
 
+     setnames(retVal,c("natrual_gas","rice","coffe", "sugar", "plat", "gold", "corn",
+                      "shanghai_al", "euronext"))
 
-    retVal = xts(retVal[, -1], order.by=as.POSIXct(retVal$Date))
+    # retVal = xts(retVal[, -1], order.by=as.POSIXct(retVal$Date))
+
+    selector = !apply(retVal, 1, function(row) any(is.na(row)))
+    retVal = data.frame(retVal[selector, ])
+
     return (retVal)
 }
